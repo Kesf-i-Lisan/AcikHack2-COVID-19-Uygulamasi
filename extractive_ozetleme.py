a2dcca9d7a6798e9c2b8990b48dbadb8699786d4
Kürşat ÇAKAL <kursat.cakal@hotmail.com>
2020-07-31 18:27:41 +0300

    Update extractive_ozetleme.py

--- extractive_ozetleme.py
+++ extractive_ozetleme.py
@@ -5,12 +5,13 @@ from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 
 
 
-class extractive_özetleme:
+class extractive_ozetleme:
     def __init__(self):
         self.dokuman = None
         self.metin = None
         self.ozet = None
         self.indis_degeri_0             = 0
+        self.indis_degeri_1             = 1
         self.derece_durum_rolesi        = 0
         self.genel_durum_txt_format     = 1
         self.genel_durum_binary_format  = 2
@@ -35,7 +36,7 @@ class extractive_özetleme:
         dokuman_cumleleri = ayristici_nesne_bileseni.tokenize(icerik)
         return dokuman_cumleleri
 
-    def özetle(self,metin):
+    def ozetle(self,metin):
         vektor_islemleri = CountVectorizer()
         vektor_islemleri_matrisi = vektor_islemleri.fit_transform(metin)
 
@@ -57,18 +58,18 @@ class extractive_özetleme:
             self.genel_islem_dizisi_tamponu.append(0)
             self.derece_durum_rolesi = 1
         if self.derece_durum_rolesi != 1:
-            for i in range(0, len(cikti_cumle_dizisi)):
-                self.genel_islem_dizisi_tamponu.append((float(cikti_cumle_dizisi[i][0]) - derece_asgari) / (derece_azami - derece_asgari))
+            for yineleyici_ekleme in range(0, len(cikti_cumle_dizisi)):
+                self.genel_islem_dizisi_tamponu.append((float(cikti_cumle_dizisi[yineleyici_ekleme][self.indis_degeri_0]) - derece_asgari) / (derece_azami - derece_asgari))
 
         uzunluklar_esigi_degeri = (sum(self.genel_islem_dizisi_tamponu) / len(self.genel_islem_dizisi_tamponu)) + 0.2
 
         ozetlenecek_cikti_cumleleri = []
         if len(self.genel_islem_dizisi_tamponu) > 1:
-            for i in range(0, len(self.genel_islem_dizisi_tamponu)):
-                if self.genel_islem_dizisi_tamponu[i] > uzunluklar_esigi_degeri:
-                    ozetlenecek_cikti_cumleleri.append(cikti_cumle_dizisi[i][1])
+            for yineleyici_ozetleme in range(0, len(self.genel_islem_dizisi_tamponu)):
+                if self.genel_islem_dizisi_tamponu[yineleyici_ozetleme] > uzunluklar_esigi_degeri:
+                    ozetlenecek_cikti_cumleleri.append(cikti_cumle_dizisi[yineleyici_ozetleme][self.indis_degeri_1])
         else:
-            ozetlenecek_cikti_cumleleri.append(cikti_cumle_dizisi[0][1])
+            ozetlenecek_cikti_cumleleri.append(cikti_cumle_dizisi[self.indis_degeri_0][self.indis_degeri_1])
 
         dokuman_ozeti = " ".join(str(x) for x in ozetlenecek_cikti_cumleleri)
         return dokuman_ozeti
@@ -76,10 +77,10 @@ class extractive_özetleme:
     def dokuman_ozetle(self,dosya_ismi):
         icerigi_yakalanan_dokuman = self.dokuman_icerigini_yakala(dosya_ismi)
         ayristirilmis_cumle_listesi = self._cumle_ayristirma_islemi(icerigi_yakalanan_dokuman)
-        self.ozet = self.özetle(ayristirilmis_cumle_listesi)
+        self.ozet = self.ozetle(ayristirilmis_cumle_listesi)
         return self.ozet
     def metin_ozetle(self,metin):
         print("burada")
         ayristirilmis_cumle_listesi = self._cumle_ayristirma_islemi(metin)
-        self.ozet = self.özetle(ayristirilmis_cumle_listesi)
+        self.ozet = self.ozetle(ayristirilmis_cumle_listesi)
         return self.ozet
